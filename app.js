95f08665c9f5c9c2416ee1003b097ebfc99caf1f
Ezra Tsai <ishow8166@icloud.com>
2020-09-08 12:26:26 +0800

    add restaurant index page

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const exphbs = require('express-handlebars')
+const exphbs  = require('express-handlebars')
 const Port = 3000
 const app = express()
 
-app.engine('hbs', exphbs({ defaultLayout: 'main'}))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-// app.get('/', (req, res) => {
-//     res.render('')
-// })
 
 app.listen(Port, () => {
     console.log(`App is now listening on Port ${Port}`)
 })
+
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
+require('./routes')(app)
