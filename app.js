2e8bd8473c56ae280c0e262c30d2633b57f0cc24
Ezra Tsai <ishow8166@icloud.com>
2020-09-08 16:41:55 +0800

    Install express-session connect-flash

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const exphbs  = require('express-handlebars')
 const db = require('./models') // 引入資料庫
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const Port = 3000
 const app = express()
 
@@ -9,6 +11,15 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+    res.locals.success_messages = req.flash('success_messages')
+    res.locals.error_messages = req.flash('error_messages')
+    next()
+})
 
 app.listen(Port, () => {
     db.sequelize.sync()
