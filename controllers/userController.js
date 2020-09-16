f720343b15b8737bb3dd5468cc86fd480187ac35
Ezra Tsai <ishow8166@icloud.com>
2020-09-16 12:54:52 +0800

    Add addFavorite and removeFavorite feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,7 @@ const db = require('../models')
 const User = db.User
 const Comment = db.Comment
 const Restaurant = db.Restaurant
+const Favorite = db.Favorite
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const userController = {
@@ -103,6 +104,29 @@ const userController = {
                 })
         }
 
+    },
+    addFavorite: (req, res) => {
+        return Favorite.create({
+            UserId: req.user.id,
+            RestaurantId: req.params.restaurantId
+        })
+            .then((restaurnat) => {
+                return res.redirect('back')
+            })
+    },
+    removeFavorite: (req, res) => {
+        return Favorite.findOne({
+            where: {
+                UserId: req.user.id,
+                RestaurantId: req.params.restaurantId
+            }
+        })
+            .then((favorete) => {
+                Favorite.destroy()
+                    .then((restaurnat) => {
+                        return res.redirect('back')
+                    })
+            })
     }
 }
 
