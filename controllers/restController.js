b870a275f750757e7d5a57094c024104c73376d7
Ezra Tsai <ishow8166@icloud.com>
2020-09-16 15:26:34 +0800

    Add like and unlike function

--- controllers/restController.js
+++ controllers/restController.js
@@ -27,8 +27,9 @@ let restController = {
             const data = result.rows.map(r => ({
                 ...r.dataValues,
                 description: r.dataValues.description.substring(0, 50),
-                // categoryName: r.dataValues.Category.name,
-                isFavorited: req.user.FavoritedRestaurants.map(d => d.id).includes(r.id)
+                categoryName: r.dataValues.Category.name,
+                isFavorited: req.user.FavoritedRestaurants.map(d => d.id).includes(r.id),
+                isLiked: req.user.LikedRestaurants.map(d => d.id).includes(r.id)
             }))
             Category.findAll({
                 raw: true,
@@ -52,13 +53,16 @@ let restController = {
             include: [
                 Category,
                 { model: User, as: 'FavoritedUsers' },
+                { model: User, as: 'LikedUsers' },
                 { model: Comment, include: [User] }
             ]
         }).then(restaurant => {
             const isFavorited = restaurant.FavoritedUsers.map(d => d.id).includes(req.user.id)
+            const isLiked = restaurant.LikedUsers.map(d => d.id).includes(req.user.id)
             return res.render('restaurant', {
                 restaurant: restaurant.toJSON(),
-                isFavorited: isFavorited
+                isFavorited: isFavorited,
+                isLiked: isLiked
             })
         })
     },
--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,7 @@ const User = db.User
 const Comment = db.Comment
 const Restaurant = db.Restaurant
 const Favorite = db.Favorite
+const Like = db.Like
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const userController = {
@@ -124,6 +125,29 @@ const userController = {
             .then((favorite) => {
                 favorite.destroy()
                     .then((restaurant) => {
+                        return res.redirect('back')
+                    })
+            })
+    },
+    addLike: (req, res) => {
+        return Like.create({
+            UserId: req.user.id,
+            RestaurantId: req.params.restaurantId
+        })
+            .then((restaurnat) => {
+                return res.redirect('back')
+            })
+    },
+    removeLike: (req, res) => {
+        return Like.findOne({
+            where: {
+                UserId: req.user.id,
+                RestaurantId: req.params.restaurantId
+            }
+        })
+            .then((like) => {
+                like.destroy()
+                    .then((restaurnat) => {
                         return res.redirect('back')
                     })
             })
