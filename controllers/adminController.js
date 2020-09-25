e63e72f35ba6ca0e00e7f72041fd5cb7cc9edf37
Ezra Tsai <ishow8166@icloud.com>
2020-09-25 11:19:50 +0800

    PUT /api/admin/restaurants/:id

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -33,50 +33,6 @@ const adminController = {
             req.flash('success_messages', data['message'])
             res.redirect('/admin/restaurants')
         })
-        // try {
-        //     if (!req.body.name) {
-        //         req.flash('error_messages', "name didn't exist")
-        //         return res.redirect('back')
-        //     }
-
-        //     const { file } = req
-        //     if (file) {
-        //         imgur.setClientID(IMGUR_CLIENT_ID);
-        //         imgur.upload(file.path, (err, img) => {
-        //             return Restaurant.create({
-        //                 name: req.body.name,
-        //                 tel: req.body.tel,
-        //                 address: req.body.address,
-        //                 opening_hours: req.body.opening_hours,
-        //                 description: req.body.description,
-        //                 image: file ? img.data.link : null,
-        //                 CategoryId: req.body.categoryId
-        //             }).then((restaurant) => {
-        //                 req.flash('success_messages', 'restaurant was successfully created')
-        //                 return res.redirect('/admin/restaurants')
-        //             })
-        //         })
-        //     }
-        //     else {
-        //         return Restaurant.create({
-        //             name: req.body.name,
-        //             tel: req.body.tel,
-        //             address: req.body.address,
-        //             opening_hours: req.body.opening_hours,
-        //             description: req.body.description,
-        //             image: null,
-        //             CategoryId: req.body.categoryId
-        //         }).then((restaurant) => {
-        //             req.flash('success_messages', 'restaurant was successfully created')
-        //             return res.redirect('/admin/restaurants')
-        //         })
-        //     }
-        // } catch (err) {
-        //     // 印出錯誤訊息
-        //     console.log(err)
-        //     // 讓程式遇到錯誤的時候，導回其他頁面
-        //     return res.redirect('/admin/restaurants')
-        // }
     },
     getRestaurant: (req, res) => {
         adminService.getRestaurant(req, res, (data) => {
@@ -100,51 +56,14 @@ const adminController = {
         })
     },
     putRestaurant: (req, res) => {
-        if (!req.body.name) {
-            req.flash('error_messages', "name didn't exist")
-            return res.redirect('back')
-        }
-
-        const { file } = req
-        if (file) {
-            imgur.setClientID(IMGUR_CLIENT_ID);
-            imgur.upload(file.path, (err, img) => {
-                return Restaurant.findByPk(req.params.id)
-                    .then((restaurant) => {
-                        restaurant.update({
-                            name: req.body.name,
-                            tel: req.body.tel,
-                            address: req.body.address,
-                            opening_hours: req.body.opening_hours,
-                            description: req.body.description,
-                            image: file ? img.data.link : restaurant.image,
-                            CategoryId: req.body.categoryId
-                        })
-                            .then((restaurant) => {
-                                req.flash('success_messages', 'restaurant was successfully to update')
-                                res.redirect('/admin/restaurants')
-                            })
-                    })
-            })
-        }
-        else {
-            return Restaurant.findByPk(req.params.id)
-                .then((restaurant) => {
-                    restaurant.update({
-                        name: req.body.name,
-                        tel: req.body.tel,
-                        address: req.body.address,
-                        opening_hours: req.body.opening_hours,
-                        description: req.body.description,
-                        image: restaurant.image,
-                        CategoryId: req.body.categoryId
-                    })
-                        .then((restaurant) => {
-                            req.flash('success_messages', 'restaurant was successfully to update')
-                            res.redirect('/admin/restaurants')
-                        })
-                })
-        }
+        adminService.putRestaurant(req, res, (data) => {
+            if (data['status'] === 'error') {
+                req.flash('error_messages', data['message'])
+                return res.redirect('back')
+            }
+            req.flash('success_messages', data['message'])
+            res.redirect('/admin/restaurants')
+        })
     },
     deleteRestaurant: (req, res) => {
         adminService.deleteRestaurant(req, res, (data) => {
